dff03fa18e55179d0c57e5f244e194b2550aec39
bsharry <maoyan32@hotmail.com>
2021-04-24 10:13:57 +0800

    Create qsort.py

---
+++ qsort.py
@@ -0,0 +1,34 @@
+import random
+def qsort(L):
+  if len(L) <= 1:
+    return
+  p = L[0]
+  low = 0
+  high = len(L) - 1
+  while low < high:
+    while low < high and p < L[high]:
+        high -= 1
+    L[low] = L[high]
+    while low < high and p > L[low]:
+        low += 1
+    L[high] =L[low]
+  L[low] = p
+  if len(L) <= 2:
+    return
+  else:
+    qsort(L[:low+1])
+    qsort(L[low+1:])
+
+b= [1,2,3,4,5,6,7,8,10]
+
+
+random.shuffle(b)
+
+print(b)
+
+qsort(b)
+
+print(b)
+
+
+
